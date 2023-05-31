76851ed0bfe6d0e85b200f6590dd3756642b92dd
JinKyong <48237998+JinKyong@users.noreply.github.com>
2023-05-31 17:31:12 +0900

    Input Package import
    유니티 자체 기능 추가

--- Assets/_Scripts/PlayerMove.cs
+++ Assets/_Scripts/PlayerMove.cs
@@ -2,17 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Vector2 inputVec;
+    Rigidbody2D body;
+
+    [SerializeField]
+    float moveSpeed;
+
     void Start()
     {
-        
+        body = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        inputVec.x = Input.GetAxisRaw("Horizontal");
+        inputVec.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        body.MovePosition(body.position + inputVec * moveSpeed * Time.deltaTime);
+    }
+
+    public void KeySetting()
+    {
+        Event keyEvent = Event.current;
     }
 }
